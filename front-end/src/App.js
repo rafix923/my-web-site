de0b6e1564de0e55225e333e65ffc9001ab49141
rafix923 <rafaellopes.dev@gmail.com>
2023-02-23 09:02:59 -0300

    feat(menu): Cria o menu para navegação entre páginas.
    O menu será composto com os seguintes items: home, serviços, portifólio, sobre e contato.
    Porém, a navegação utilizará a navegação pelo react-router-dom.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import Header from './components/Header/Header'
 
 export default function App() {
   return (
-    <div>app</div>
+    <div>
+      <Header/>
+    </div>
   )
 }
 
---
+++ front-end/src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Header() {  
+
+    return (
+        <nav>
+        <ul>
+            <li>Home</li>
+            <li>Serviços</li>
+            <li>Portifólio</li>
+            <li>Sobre</li>
+            <li>Contato</li>
+        </ul>
+        </nav>
+    )
+}
