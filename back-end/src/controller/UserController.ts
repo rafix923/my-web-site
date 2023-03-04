30446a9f06bd4c582121e7283f5715faafd4f4df
rafix923 <rafaellopes.dev@gmail.com>
2023-03-04 12:25:07 -0300

    feat(controller): Cria a camada controller e dentro dela a classe UserController.
    A camada controller ficará responsável por receber os dados da requisição, interagir com a camada business e devolver a resposta da requisição.

---
+++ back-end/src/controller/UserController.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from "express";
+import { UserBusiness } from "../buniness/UserBusiness";
+
+export class UserController {
+
+    createUser = async (req: Request, res: Response): Promise<void> => {
+        try {
+            const input: any = {
+                name: req.body.nome,
+                email: req.body.email,
+                phone: req.body.telefone,
+                details: req.body.detalhes
+            };
+
+            const userBusiness = new UserBusiness()
+            await userBusiness.createUser(input)
+
+            res.status(201).send({ message: "Suas informações foram recebidas. Logo entraremos em contato!" })
+        } catch (error: any) {
+            res.status(400).send(error.message)
+        }
+    };
+};
