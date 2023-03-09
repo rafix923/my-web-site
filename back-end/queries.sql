cbb197ceaedac01e73f817950286e0dae5e52c3a
rafix923 <rafaellopes.dev@gmail.com>
2023-03-09 18:59:51 -0300

    refactore(queries): Refatora o id para primari key, desfazendo a manipulação do id.

--- back-end/queries.sql
+++ back-end/queries.sql
@@ -1,11 +1,8 @@
-
 CREATE TABLE
     webpage_user(
-        id VARCHAR(255),
+        id VARCHAR(255) PRIMARY KEY,
         name VARCHAR(255) NOT NULL,
         email VARCHAR(255) UNIQUE NOT NULL,
         phone VARCHAR(255) UNIQUE NOT NULL,
         details TEXT,
-        PRIMARY KEY (id)
     );
-
