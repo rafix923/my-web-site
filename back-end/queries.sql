46b6d559fecab36a388e418caa416434cb009d69
rafix923 <rafaellopes.dev@gmail.com>
2023-03-13 19:53:21 -0300

    test: Testa a criação de usuários.

--- back-end/queries.sql
+++ back-end/queries.sql
@@ -1,8 +1,9 @@
+-- Active: 1678732890233@@127.0.0.1@3306@webpage
 CREATE TABLE
     webpage_user(
         id VARCHAR(255) PRIMARY KEY,
         name VARCHAR(255) NOT NULL,
         email VARCHAR(255) UNIQUE NOT NULL,
         phone VARCHAR(255) UNIQUE NOT NULL,
-        details TEXT,
+        details TEXT
     );
